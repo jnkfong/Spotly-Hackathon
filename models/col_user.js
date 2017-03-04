a65201f599af53b2e7d3ca6689fe8c47f18c33c8
James Fong <James Fong>
2017-03-04 17:15:57 -0500

    added models folder

---
+++ models/col_user.js
@@ -0,0 +1,15 @@
+onst mongoose = require('mongoose');
+const crypto = require('crypto');
+
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+  firstname: { type: String, default: '' },
+  lastname: { type: String, default: '' },
+  email: { type: String, default: '' },
+  username: { type: String, default: '' },
+  location: { type: String, default: '' },
+  password: { type: String, default: '' }
+ 
+});
+
+mongoose.model('User', UserSchema);
