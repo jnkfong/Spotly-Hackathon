f2f12ad56bba371843a8941120e1971201cef4a8
Matthew Fung <matthewfung@Matthews-MacBook-Pro.local>
2017-03-04 17:01:52 -0500

    Deleted database

--- server.js
+++ server.js
@@ -1,28 +1,37 @@
+//Includes
 var express = require('express')
 var app = express()
-var db = require('database.js')
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/27017');
 
+//Views
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+//Run server
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
+//Connect to DB
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('were connected');
+});
 
-// Establish connection to db
-db.open(function(err, db) {
-  assert.equal(null, err);
-	console.log('Database opened!');
-	db.collection('users', function(err, collection) {
-		if (err) {
-			throw err;
-		} else {
-			collection.find({password:"123"}, function(err, document) {
-			console.log(document);
-			});
-		}
-		db.close();
-	});
-  });
+// //write query here
+// app.get("/get_users", function(req, res) {
+//         var body = req.body;
+//         // db.getUserByID(body.ID, function(err, data){
+//         db.getUserByID(1, function(err, data){
+//             if (err) {
+//                 console.log(err);
+//                 return res(err);
+//             } else {
+//                 console.log(data);
+//                 return res.json(data);
+//             }
+//         });
+//     });
