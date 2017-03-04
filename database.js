e69b744632861ca5a23bc3406107e98a7ea7f5d7
Matthew Fung <matthewfung@Matthews-MacBook-Pro.local>
2017-03-04 15:28:34 -0500

    Isolated Database code, cleaned up server code

--- database.js
+++ database.js
@@ -1,1 +1,13 @@
 //database code goes here
+var Db = require('mongodb').Db,
+    MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server,
+    ReplSetServers = require('mongodb').ReplSetServers,
+    ObjectID = require('mongodb').ObjectID,
+    Binary = require('mongodb').Binary,
+    GridStore = require('mongodb').GridStore,
+    Code = require('mongodb').Code,
+    assert = require('assert');
+
+var db = new Db('integration_tests', new Server("127.0.0.1", 27017,
+ {auto_reconnect: false, poolSize: 4}), {w:0, native_parser: false});
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var db = require('database.js')
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -9,24 +10,12 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
-var Db = require('mongodb').Db,
-    MongoClient = require('mongodb').MongoClient,
-    Server = require('mongodb').Server,
-    ReplSetServers = require('mongodb').ReplSetServers,
-    ObjectID = require('mongodb').ObjectID,
-    Binary = require('mongodb').Binary,
-    GridStore = require('mongodb').GridStore,
-    Code = require('mongodb').Code,
-    assert = require('assert');
-
-var db = new Db('integration_tests', new Server("127.0.0.1", 27017,
- {auto_reconnect: false, poolSize: 4}), {w:0, native_parser: false});
 
 // Establish connection to db
 db.open(function(err, db) {
   assert.equal(null, err);
 	console.log('Database opened!');
-	db.collection('chat', function(err, collection) {
+	db.collection('users', function(err, collection) {
 		if (err) {
 			throw err;
 		} else {
@@ -35,15 +24,5 @@ db.open(function(err, db) {
 			});
 		}
 		db.close();
-	}); 
+	});
   });
-
-
-//var http = require('http');
-/*
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
-*/
