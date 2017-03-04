31a8316fefea4e1adb4954d9b4bb3699305f9b69
James Fong <James Fong>
2017-03-04 15:06:18 -0500

    Initial Start Spotly <need to connect express with mongo>
    Signed-off-by: James Fong <James Fong>

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
+var Db = require('mongodb').Db,
+    MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server,
+    ReplSetServers = require('mongodb').ReplSetServers,
+    ObjectID = require('mongodb').ObjectID,
+    Binary = require('mongodb').Binary,
+    GridStore = require('mongodb').GridStore,
+    Code = require('mongodb').Code,
+    assert = require('assert');
+
+var db = new Db('integration_tests', new Server("127.0.0.1", 27017,
+ {auto_reconnect: false, poolSize: 4}), {w:0, native_parser: false});
+
+// Establish connection to db
+db.open(function(err, db) {
+  assert.equal(null, err);
+	console.log('Database opened!');
+	db.collection('chat', function(err, collection) {
+		if (err) {
+			throw err;
+		} else {
+			collection.find({password:"123"}, function(err, document) {
+			console.log(document);
+			});
+		}
+		db.close();
+	}); 
+  });
+
+
+//var http = require('http');
+/*
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+*/
