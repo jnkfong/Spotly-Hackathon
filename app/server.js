aba9f8985d980b77ac9dbea22d9e283ce9f363f8
Matthew Fung <matthewfung@Matthews-MacBook-Pro.local>
2017-03-04 19:27:49 -0500

    FINALLY GOT MONGOOSE WORKING WITH DB

--- app/server.js
+++ app/server.js
@@ -1,22 +1,31 @@
 //Includes
-var express = require('express')
-var app = express()
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/27017');
+var express = require('express');
+var app = express();
+const mongoose = require('mongoose');
+
+//connect to DB
+var uri ='mongodb://localhost:27017';
+var db = mongoose.connect(uri);
+
+var User = require('../models/col_user.js');
+
+//register new user
+// var user = new User({firstname:'Matt'});
+// user.save();
 
 //Views
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.get('/users',function(req,res){
+  mongoose.model('User').find(function(err,user){
+  res.send(user);
+});
+});
+
 //Run server
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('-Server running-')
 
-//Connect to DB
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('were connected');
-});
+})
